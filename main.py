f8ec5dde262d8664becc19db2d89d2d20491d10d
muyi2905 <obaremimuyiwa@gmail.com>
2023-10-02 11:18:57 +0000

    commit

--- main.py
+++ main.py
@@ -7,3 +7,29 @@ import time
 
 # Amazon product URL
 url = 'https://www.amazon.com/Apple-MacBook-13-inch-Storage-English/dp/B0CB745VMN/ref=sr_1_3?crid=KOSKD8OIB9HJ&keywords=macbook+pro&qid=1696241550&sprefix=macbook+pr%2Caps%2C558&sr=8-3'
+
+
+# Set your target price
+target_price = 700.0  # Replace with the desired target price
+
+# Gmail configuration (you will need to use your own Gmail account)
+sender_email = 'your_email@gmail.com'
+sender_password = 'your_password'
+receiver_email = 'receiver_email@gmail.com'  # Replace with the recipient's email address
+
+def get_amazon_product_price(url):
+    headers = {
+        "User-Agent": "Your User Agent Here"  # Replace with your user agent
+    }
+    
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Locate the price element on the Amazon product page
+    price_element = soup.find('span', {'id': 'priceblock_ourprice'})
+
+    if price_element:
+        price = float(price_element.get_text().replace('$', '').replace(',', ''))
+        return price
+    else:
+        return None
