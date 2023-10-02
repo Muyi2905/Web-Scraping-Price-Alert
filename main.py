2c3f511ca31cf30618c513d9764576f720616612
muyi2905 <obaremimuyiwa@gmail.com>
2023-10-02 11:17:15 +0000

    linked amazon product url

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ import requests
 from bs4 import BeautifulSoup
 import smtplib
 import time
+
+# Amazon product URL
+url = 'https://www.amazon.com/Apple-MacBook-13-inch-Storage-English/dp/B0CB745VMN/ref=sr_1_3?crid=KOSKD8OIB9HJ&keywords=macbook+pro&qid=1696241550&sprefix=macbook+pr%2Caps%2C558&sr=8-3'
